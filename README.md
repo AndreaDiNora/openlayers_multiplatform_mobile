4505dc508f821e4612a68f81d63619997d8506c5
Andrea Di Nora <Seldoran@gmail.com>
2019-04-07 20:40:06 +0200

    First Commit of example

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # Openlayers Multiplatform Mobile
 Test application that uses Cordova to create an Openlayers map on different mobile and web platforms
+
+Follow this guideline on Cordova to start with the project.
+
+After, you can use the command 
+
+cordova run browser 
+
+to start app on your browser
+
+or
+
+cordova run android 
+
+to emulate your app on an Android device (i connect it with my physical device)
+
+
+Here a <a href="https://www.dnavideochannel.net/demos/OL-Multiplatform/demo-Openlayers-Multi-Platform.html">live demo</a> for browser version.
+
+
+
---
+++ www/js/index.js
@@ -0,0 +1,66 @@
+/*
+MIT License
+
+Copyright (c) 2019 Andrea Di Nora
+web site : https://www.dnavideochannel.net
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+ */
+var app = {
+    // Application Constructor
+    initialize: function() {
+        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+    },
+
+    // deviceready Event Handler
+    //
+    // Bind any cordova events here. Common events are:
+    // 'pause', 'resume', etc.
+    onDeviceReady: function() {
+        this.receivedEvent('deviceready');
+	    
+      var map = new ol.Map({
+        target: 'map',
+        layers: [
+          new ol.layer.Tile({
+            source: new ol.source.OSM()
+          })
+        ],
+        view: new ol.View({
+          center: ol.proj.fromLonLat([14.12, 42.06]),
+          zoom: 6
+        })
+      });	    
+    },
+
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    }
+};
+
+app.initialize();
